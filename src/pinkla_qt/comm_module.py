d8f4334baac11db09dd21b912c331095611900e2
gudxok <gudxok_@naver.com>
2024-03-27 16:46:41 +0900

    class move to module

--- src/pinkla_qt/comm_module.py
+++ src/pinkla_qt/comm_module.py
@@ -1,13 +1,23 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+from PyQt5.QtCore import *
+
+import cv2, imutils
 import socket
-import cv2
 import numpy as np
 import serial
+
+import time
+# import datetime
 from datetime import datetime
+import psutil
 
 cmtx = np.array([[474.9308089, 0, 313.10372736],
                 [0, 474.24684641, 254.94399015],
                 [0,           0,            1]])
 dist = np.array([[0.0268074362, -0.178310961, -0.000144841081, -0.00103575477, 0.183767484]])
+
 
 class SERVER():
     def __init__(self, host='0.0.0.0', port=0000):
@@ -21,9 +31,9 @@ class SERVER():
         try:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.s.bind((self.host, self.port))
-            self.s.listen(2)
-
+            self.s.listen(10)
         except Exception as e:
+            print(e)
             pass
 
     def disconnect(self):
@@ -67,7 +77,7 @@ class SERVER():
             fourcc = cv2.VideoWriter_fourcc(*'XVID')
             self.writer = cv2.VideoWriter(video_filename, fourcc, fps, (width, height))
         except Exception as e:
-            # print(e)
+            print(e)
             return None
 
     def record_stop(self):
@@ -90,3 +100,163 @@ class CLIENT():
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except Exception as e:
             print(e)
+
+
+class Socket(QThread):
+    update = pyqtSignal(object)
+
+    def __init__(self, host, port):
+        super().__init__()
+        self.running = True
+        self.host = host
+        self.port = port
+        self.conn, self.addr = None, None
+        self.server = SERVER(self.host, self.port)
+
+    def run(self):
+        self.server.connect()
+        self.server.s.settimeout(0.1)
+        while self.running:
+            try:
+                self.conn, self.addr = self.server.s.accept()
+                if self.conn:
+                    self.update.emit(self.conn)
+                    self.running = False
+            except socket.timeout:
+                pass
+            except Exception as e:
+                pass
+    
+    def disconnect(self):
+        try:
+            self.conn.close()
+            self.server.s.close()
+            self.update.emit(False)
+            # del self.conn
+            # del self.server.s
+            QThread.msleep(100)
+        except Exception as e:
+            pass
+
+    def stop(self):
+        print("stop connection.")
+        self.running = False
+        try:
+            self.disconnect()
+        except Exception as e:
+            pass
+
+
+class Camera_Th(QThread):
+    update = pyqtSignal(QPixmap)
+
+    def __init__(self, sec=0, parent=None):
+        super().__init__()
+        self.main = parent
+        self.running = True
+
+        self.cam_server = SERVER()
+        self.conn = None
+        self.source, self.pixmap = None, None
+
+    def run(self):
+        # print(self.conn)
+        while self.conn is None:
+            QThread.msleep(10)
+        self.cam_server.conn = self.conn
+
+        while self.running == True:
+            self.source = self.cam_server.show_video()
+            if self.source is not None:
+                image = cv2.cvtColor(self.source, cv2.COLOR_BGR2RGB) 
+                h,w,c = image.shape
+                qformat_type = QImage.Format_RGB888
+                qimage = QImage(image.data, w, h, w*c, qformat_type)
+                self.pixmap = QPixmap.fromImage(qimage)
+                self.update.emit(self.pixmap)
+            QThread.msleep(8)
+
+    def stop(self):
+        self.running = False
+
+
+class Socket_Pinkla(QThread):
+    update = pyqtSignal(object)
+    def __init__(self, host, port):
+        super().__init__()
+        self.running = True
+        self.host = host
+        self.port = port
+        self.conn, self.addr = None, None
+        self.client = CLIENT(self.host, self.port)
+        self.s = None
+
+    def run(self):
+        self.client.connect()
+        connected = False
+        self.s = self.client.s
+        while self.running:
+            try:
+                self.s.connect((self.host, self.port))
+                connected = True
+                if connected:
+                    self.running = False
+                    self.update.emit(self.s)
+            except ConnectionRefusedError:
+                print("Waiting for server...")
+                connected = False
+                QThread.msleep(1000)
+                pass
+            except Exception as e:
+                connected = False
+                print(e)
+                pass
+    
+    def disconnect(self):
+        try:
+            self.s.close()
+            self.update.emit(False)
+            del self.s
+            QThread.msleep(100)
+        except Exception as e:
+            pass
+
+    def stop(self):
+        print("stop connection.")
+        self.running = False
+        try:
+            self.disconnect()
+        except Exception as e:
+            pass
+
+
+class Control_Pinkla(QThread):
+    update = pyqtSignal(bool)
+
+    def __init__(self, s=None):
+        super().__init__()
+        self.running = True
+        self.cmd = [0, 5, 0, 0, 0, 0]
+        self.s = s
+
+    def run(self):
+        while self.running:
+            try:
+                data_str = ','.join(map(str, self.cmd))
+                self.s.sendall(data_str.encode())
+
+                self.update.emit(True)
+                QThread.msleep(20)
+            except Exception as e :
+                print(e)
+                self.s.close()
+                self.update.emit(False)
+                self.running = False
+                pass
+
+    def stop(self):
+        self.running = False
+        try:
+            self.s.close()
+        except Exception as e:
+            pass
