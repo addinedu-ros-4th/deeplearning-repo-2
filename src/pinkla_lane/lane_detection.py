5610916ecbcf94907bfb3d39887d05972d3f6bce
gudxok <gudxok_@naver.com>
2024-04-02 01:47:01 +0900

    roi fixing, optimizing

--- src/pinkla_lane/lane_detection.py
+++ src/pinkla_lane/lane_detection.py
@@ -61,6 +61,12 @@ class find_load_center():
         self.img_center_x = int(640/2)
         self.img_center_y = int(480/2)
 
+        self.roi_rect_start = (0, self.img_center_y - 100)
+        self.roi_rect_end = (self.img_center_x * 2, int(self.img_center_y * 1.99))
+        # roi_rect_center_y = (int(self.roi_rect_start[1]+self.roi_rect_end[1])/2)
+        # roi_upside = [(0, 0), (self.img_center_x*2, int(self.img_center_y)-130)]
+        # roi_downside = ((0, int(self.img_center_y * 1.75)), (self.img_center_x*2, self.img_center_y*2))
+
         self.seg_center_middle = (0,0)
         self.seg_center_border = (0,0)
         self.seg_center_inter = (0,0)
@@ -74,10 +80,11 @@ class find_load_center():
         self.alpha = 0.3
         self.error = 0
 
-        self.isDetectMiddleLine = False
         self.status = ""
-        
- 
+
+        self.zeros_image = np.zeros((480, 640, 3)).astype(np.uint8)
+        self.zeros_image[:, :] = [0, 0, 255]
+
     def get_road_center(self, image):
         self.seg_center_middle_list = []
         self.seg_center_border_list = []
@@ -85,49 +92,42 @@ class find_load_center():
 
         self.image = image.copy()
         
-        roi_rect_start = (0, int(self.img_center_y)-130)
-        roi_rect_end = (self.img_center_x * 2, int(self.img_center_y * 1.75))
-
-        roi_rect_center_y = (int(roi_rect_start[1]+roi_rect_end[1])/2)
-        roi_upside = [(0, 0), (self.img_center_x*2, int(self.img_center_y)-130)]
-        roi_downside = ((0, int(self.img_center_y * 1.75)), (self.img_center_x*2, self.img_center_y*2))
-
-        zeros_image = np.zeros((480, 640, 3)).astype(np.uint8)
-        zeros_image[:, :] = [0, 0, 255]
+        ROI = self.image[self.roi_rect_start[1]:self.roi_rect_end[1], self.roi_rect_start[0]:self.roi_rect_end[0]]
+        self.zeros_image[self.roi_rect_start[1]:self.roi_rect_end[1], self.roi_rect_start[0]:self.roi_rect_end[0]] = ROI
         
-        ROI = self.image[roi_rect_start[1]:roi_rect_end[1], roi_rect_start[0]:roi_rect_end[0]]
-        zeros_image[roi_rect_start[1]:roi_rect_end[1], roi_rect_start[0]:roi_rect_end[0]] = ROI
-        
-        self.result = self.model.predict(source = zeros_image, conf=0.5, verbose=False)[0]
+        self.result = self.model.predict(source = self.zeros_image, conf=0.4, verbose=False)[0]
         self.classes = self.result.boxes
         self.segmentation = self.result.masks
         
         try: 
-            for mask, box in zip(self.segmentation,self.classes):
+            for mask, box in zip(self.segmentation, self.classes):
+                # border line
                 if box.cls.item()==0:
                     xy = mask.xy[0].astype("int")
                     if check_right_lane(xy):
-                        cv2.polylines(self.image,[xy],isClosed=True,color=(255,255,255),thickness=2)
+                        centroid = np.mean(xy, axis=0).astype(int)
+                        cv2.putText(self.image, text="test", org=(centroid[0], centroid[1]), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,255,255), thickness=2)
+                        cv2.polylines(self.image,[xy],isClosed=True,color=(255,255,255),thickness=3)
                         self.seg_center_border = get_centroid(xy)
                         self.seg_center_border_list.append(self.seg_center_border)
 
                 # intersection
                 if box.cls.item()==1: 
                     xy = mask.xy[0].astype("int") 
-                    if check_left_lane(xy):
-                        cv2.polylines(self.image,[xy],isClosed=True,color=(0,0,255),thickness=2)
-                        self.seg_center_inter = get_centroid(xy)
-                        self.seg_center_inter_list.append(self.seg_center_inter)
+                    cv2.polylines(self.image,[xy],isClosed=True,color=(0,0,255),thickness=3)
+                    self.seg_center_inter = get_centroid(xy)
+                    self.seg_center_inter_list.append(self.seg_center_inter)
 
                 # middle_line
                 if box.cls.item()==2:
                     xy = mask.xy[0].astype("int")
-                    cv2.polylines(self.image,[xy],isClosed=True,color=(0,255,255),thickness=2)
-                    self.seg_center_middle = get_centroid(xy)
-                    self.seg_center_middle_list.append(self.seg_center_middle)
+                    if check_left_lane(xy):
+                        cv2.polylines(self.image,[xy],isClosed=True,color=(0,255,255),thickness=3)
+                        self.seg_center_middle = get_centroid(xy)
+                        self.seg_center_middle_list.append(self.seg_center_middle)
 
         except TypeError as e:
-            print("for mask, box in zip(self.segmentation,self.classes) : ",e)
+            print("for mask, box in zip(self.segmentation,self.classes) : ", e)
             pass
         
         # middle line, intersection
@@ -140,17 +140,17 @@ class find_load_center():
                 self.line_center = self.seg_center_inter_list[0]
             else:
                 self.status = "None"
-                self.line_center = self.seg_center_middle
+                self.line_center = self.seg_center_border
 
-        road_center = (int((self.line_center[0] + self.seg_center_border[0])/2), int(self.img_center_y*1.5))        
+        road_center = (int((self.line_center[0] + self.seg_center_border[0])/2), int(self.img_center_y*1.5))
 
-        cv2.rectangle(self.image, roi_rect_start, roi_rect_end, color = (255, 0, 0), thickness = 5)
-        cv2.line(self.image, (self.line_center[0], int(self.img_center_y*1.5)), (self.seg_center_border[0], int(self.img_center_y*1.5)), color = (0, 255, 0), thickness=2)
+        cv2.rectangle(self.image, self.roi_rect_start, self.roi_rect_end, color = (255, 0, 0), thickness = 8)
+        cv2.line(self.image, (self.line_center[0], road_center[1]), (self.seg_center_border[0], road_center[1]), color = (0, 255, 0), thickness=2)
         cv2.circle(self.image, road_center, radius = 3, color = (0, 0, 255), thickness = -1)
-        cv2.circle(self.image, (self.img_center_x, int(self.img_center_y*1.5)), radius = 5, color = (255, 255, 255), thickness = -1)
+        cv2.circle(self.image, (self.img_center_x, road_center[1]), radius = 5, color = (255, 255, 255), thickness = -1)
         
         self.error = road_center[0]-(self.img_center_x)
 
-        # print(self.error)
+        print(self.status, self.line_center, road_center, self.error)
         # print(self.line_center)
         return self.image, self.error
