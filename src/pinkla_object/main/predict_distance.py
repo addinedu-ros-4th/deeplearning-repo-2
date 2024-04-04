b763ed0090f047ee8364be69186c8cd1c406b92e
IMddubin <lsv2620@gmail.com>
2024-04-04 16:04:49 +0900

    modify path, calibration

--- src/pinkla_object/main/predict_distance.py
+++ src/pinkla_object/main/predict_distance.py
@@ -10,18 +10,27 @@ class find_object():
         self.names = self.model.model.names
     # 실제 객체들의 너비값을 저장(지정된 객체만 검출 됨 / 단위 : inch)
         self.known_widths = {
-            'minions' : 1.97,
-            'tank' : 3.54
-        }
+            'car' : 4,
+            'crosswalk' : 6.496,
+            'green_light' : 1.969, # 1.378
+            'human' : 1.969,
+            'only_right_turn' : 2.36,
+            'only_straight' : 2.36,
+            'red_light' : 1.969,
+            'start_line' : 7.087,
+            'stop_line' : 4.331,
+            'yellow_light' : 1.969
+            }
         # self.KNOWN_DISTANCE = 11.81  # 객체와의 거리(초기에 검출되는 객체에 한해 입력 요구 / 단위 : inch)
         # KNOWN_WIDTH = 6.7  # 실제 객체의 너비
 
-        self.focalLength = 694.55
+        self.focalLength = 478.87
 
     def get_bounding_boxes(self, results):
         # yolo 모델의 예측 결과에서 객체 boundary box와 클래스 명을 반환
         self.object_boxes = [] # 객체 bounding box를 저장할 리스트
         self.classes = [] # 클래스 명을 저장할 리스트
+        self.confidences = []
 
         for result in results:
             bounding_boxes = result.boxes
@@ -30,10 +39,12 @@ class find_object():
                 x1, y1, x2, y2 = box.xyxy[0] # 바운딩 박스의 좌표 정보 추출
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 cls = int(box.cls[0]) # 바운딩 박스에 대한 클래스 정보 추출
+                conf = float(box.conf[0])
                 self.object_boxes.append([x1, y1, x2, y2])
                 self.classes.append(cls)
+                self.confidences.append(conf)
 
-        return self.object_boxes, self.classes
+        return self.object_boxes, self.classes, self.confidences
 
     def distance_to_camera(self, knownWidth, focalLength, perWidth):
 
@@ -46,17 +57,10 @@ class find_object():
 
     def calculate_depth(self, image):
         
-        # mtx = np.array([[479.02325822, 0., 300.28563966],
-        #                 [0., 478.72152516, 232.33223961],
-        #                 [0., 0., 1.]])
-        # dist = np.array([[-0.01884809, 0.00845537, -0.00234588, -0.00259763, -0.06820042]])
-        # mtx = cmtx1 
-        # dist = dist1
-
-        mtx = np.array([[693.79303454, 0. ,290.04788772],
-                  [0., 695.31755488, 248.75886213], 
-                  [0.,0.,1.]])
-        dist = np.array([[-0.41231278, 0.15145024, 0.00230435, 0.00253762, 0.13721517]])
+        mtx = np.array([[479.02325822, 0., 300.28563966],
+                        [0., 478.72152516, 232.33223961],
+                        [0., 0., 1.]])
+        dist = np.array([[-0.01884809, 0.00845537, -0.00234588, -0.00259763, -0.06820042]])
 
         self.image = image.copy()
 
@@ -65,14 +69,14 @@ class find_object():
         # vid_stride : 연속된 프레임 사이 간격
         # 최대 감지 객체 개수 제한
         # verbose=False : 터미널 출력 제한
-        results = self.model.predict(self.calibrated_image, conf=0.6, vid_stride=30, max_det = 5, verbose=False)
+        results = self.model.predict(self.calibrated_image, conf=0.6, vid_stride=30, max_det = 10, verbose=False)
         # results = model.predict(source=image, conf=0.5)[0]
 
         # 첫 번째 프레임에서 객체가 감지된 경우
         if results[0].boxes is not None:
-            object_boxes, cls_indices = self.get_bounding_boxes(results)
+            object_boxes, cls_indices, confidences = self.get_bounding_boxes(results)
 
-            for object_box, cls_index in zip(object_boxes, cls_indices):
+            for object_box, cls_index, confidence in zip(object_boxes, cls_indices, confidences):
                 class_name = self.names[cls_index]
 
                 if class_name in self.known_widths:
@@ -82,7 +86,7 @@ class find_object():
                     distance = self.distance_to_camera(known_width, self.focalLength, perwidth)
                     # 바운딩 박스와 거리 정보 영상에 표시
                     cv2.rectangle(self.calibrated_image, (object_box[0], object_box[1]), (object_box[2], object_box[3]), (255,0,255), 2)
-                    cv2.putText(self.calibrated_image, f"{(distance*2.278):.2f}cm", (object_box[0], object_box[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+                    cv2.putText(self.calibrated_image, f"{(distance*2.5126):.2f}cm, Conf: {confidence:.2f}", (object_box[0], object_box[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
                     # 클래스명 출력
                     cv2.putText(self.calibrated_image, class_name, (object_box[0], object_box[1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
 
