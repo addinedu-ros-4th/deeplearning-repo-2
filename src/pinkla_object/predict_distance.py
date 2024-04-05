0fcae6b95cadd17eeab50a3c729a0ca4a0ae91bc
IMddubin <lsv2620@gmail.com>
2024-04-05 11:04:32 +0900

    modify predict_distance.py

--- src/pinkla_object/predict_distance.py
+++ src/pinkla_object/predict_distance.py
@@ -6,23 +6,42 @@ import numpy as np
 
 class Find_Object():
     def __init__(self):
-        self.model = YOLO('../../data/best_yolos_object.pt')
+        self.model = YOLO('../../data/bestobjectv8n.pt')
         self.names = self.model.model.names
     # 실제 객체들의 너비값을 저장(지정된 객체만 검출 됨 / 단위 : inch)
         self.known_widths = {
-            'minions' : 1.97,
-            'tank' : 3.54
+            'car' : 3.543,
+            'crosswalk' : 6.496,
+            'green_light' : 1.654, # 1.378
+            'human' : 2.165,
+            'only_right_turn' : 2.165,
+            'only_straight' : 2.165,
+            'red_light' : 1.654,
+            'start_line' : 7.48,
+            'stop_line' : 4.724,
+            'yellow_light' : 1.654
         }
-        # self.KNOWN_DISTANCE = 11.81  # 객체와의 거리(초기에 검출되는 객체에 한해 입력 요구 / 단위 : inch)
-        # KNOWN_WIDTH = 6.7  # 실제 객체의 너비
 
-        # self.focalLength = 478.87
+        self.colors = {
+            'car': (0, 111, 0), 
+            'crosswalk': (140, 140, 140), 
+            'green_light': (22, 219, 29),
+            'human': (0, 228, 255), 
+            'only_right_turn': (255, 216, 0),
+            'only_straight': (255, 0, 95),
+            'red_light': (95, 95, 241), 
+            'start_line': (255, 165, 0),
+            'stop_line': (0, 94, 255), 
+            'yellow_light': (125, 237, 250)
+        }
+
         self.focalLength = (470.86256773 + 470.89842857) / 2.0
 
     def get_bounding_boxes(self, results):
         # yolo 모델의 예측 결과에서 객체 boundary box와 클래스 명을 반환
         self.object_boxes = [] # 객체 bounding box를 저장할 리스트
         self.classes = [] # 클래스 명을 저장할 리스트
+        self.confidences = []
 
         for result in results:
             bounding_boxes = result.boxes
@@ -31,10 +50,12 @@ class Find_Object():
                 x1, y1, x2, y2 = box.xyxy[0] # 바운딩 박스의 좌표 정보 추출
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 cls = int(box.cls[0]) # 바운딩 박스에 대한 클래스 정보 추출
+                conf = float(box.conf[0]) # 바운딩 박스에 대한 confidence 점수 추출
                 self.object_boxes.append([x1, y1, x2, y2])
                 self.classes.append(cls)
+                self.confidences.append(conf)
 
-        return self.object_boxes, self.classes
+        return self.object_boxes, self.classes, self.confidences
 
     def distance_to_camera(self, knownWidth, focalLength, perWidth):
 
@@ -52,21 +73,24 @@ class Find_Object():
 
         # 첫 번째 프레임에서 객체가 감지된 경우
         if results[0].boxes is not None:
-            object_boxes, cls_indices = self.get_bounding_boxes(results)
+            object_boxes, cls_indices, confidences = self.get_bounding_boxes(results)
 
-            for object_box, cls_index in zip(object_boxes, cls_indices):
+            for object_box, cls_index, confidence in zip(object_boxes, cls_indices, confidences):
                 class_name = self.names[cls_index]
-
                 if class_name in self.known_widths:
                     known_width = self.known_widths[class_name]
+                    color = self.colors[class_name]
+
                     perwidth = object_box[2] - object_box[0] # (x2 - x1)
                     # 객체와 카메라 사이의 거리 계산
                     distance = self.distance_to_camera(known_width, self.focalLength, perwidth)
+                    distance = distance*2.278
                     # 바운딩 박스와 거리 정보 영상에 표시
-                    cv2.rectangle(self.image, (object_box[0], object_box[1]), (object_box[2], object_box[3]), (255,0,255), 2)
-                    cv2.putText(self.image, f"{(distance*2.278):.2f}cm", (object_box[0], object_box[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+                    cv2.rectangle(self.image, (object_box[0], object_box[1]), (object_box[2], object_box[3]), color, 2)
+                    cv2.putText(self.image, f"{(distance*1.956):.2f}cm", (object_box[2] - 50, object_box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 0, 0), 2)
+                    cv2.putText(self.image, f"Conf: {confidence:.2f}", (object_box[0], object_box[3] + 20), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 255, 0), 2)
+                    
                     # 클래스명 출력
-                    cv2.putText(self.image, class_name, (object_box[0], object_box[1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
+                    cv2.putText(self.image, class_name, (object_box[0], object_box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, color, 2)
 
         return self.image
-            
