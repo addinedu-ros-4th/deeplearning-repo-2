13f5806a6e3160813a51f20f5b86eebecc69395d
gudxok <gudxok_@naver.com>
2024-04-05 16:16:30 +0900

    camera connection bug fix

--- src/pinkla_camera/pinkla_camera.py
+++ src/pinkla_camera/pinkla_camera.py
@@ -4,8 +4,9 @@ import numpy as np
 import time
 import threading
 import sys
+import copy
 
-hard_cam_index = [0, 1, 2, 3]
+hard_cam_index = ["0", "1", "2", "3"]
 
 class CameraStreamer:
     def __init__(self, server_address, port, cam_index=0, width=640, height=480, quality=100):
@@ -18,14 +19,6 @@ class CameraStreamer:
         self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
         self.socket = None
         
-        if self.cam_index not in hard_cam_index:
-            self.cam = cv2.VideoCapture(self.cam_index)
-            self.cam.set(3, self.width)
-            self.cam.set(4, self.height)
-        else:
-            self.cam = self.cam_index
-
-
     def connect_to_server(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         connected = False
@@ -41,6 +34,15 @@ class CameraStreamer:
 
     def send_video_data(self):
         self.socket = self.connect_to_server()
+
+        if self.cam_index in hard_cam_index:
+            self.cam = cv2.VideoCapture(int(self.cam_index))
+            self.cam.set(3, self.width)
+            self.cam.set(4, self.height)
+        else:
+            self.cam = cv2.VideoCapture(self.cam_index)
+            self.cam.set(3, self.width)
+            self.cam.set(4, self.height)
 
         while True:
             try:
@@ -68,20 +70,22 @@ def main():
     # Check the number of arguments
     if len(sys.argv) == 2:
         # Only one argument, run a single thread for the specified camera index
-        cam_index = int(sys.argv[1])
-        # cam_index = sys.argv[1]
+        try:
+            cam_index = sys.argv[1]
+        except ValueError:
+            cam_index = str(sys.argv[1])
+
         camera1 = CameraStreamer(server_address, port1, cam_index)
-        camera2 = CameraStreamer(server_address, port2, camera1.cam)
         thread1 = threading.Thread(target=camera1.send_video_data)
-        thread2 = threading.Thread(target=camera2.send_video_data)
         thread1.start()
-        thread2.start()
         thread1.join()
-        thread2.join()
 
     elif len(sys.argv) == 3:
         # Two arguments, run threads for both camera indices
-        cam_indices = [int(arg) for arg in sys.argv[1:]]
+        try:
+            cam_indices = [arg for arg in sys.argv[1:]]
+        except ValueError:
+            cam_indices = [str(arg) for arg in sys.argv[1:]]
 
         camera1 = CameraStreamer(server_address, port1, cam_indices[0])
         camera2 = CameraStreamer(server_address, port2, cam_indices[1])
