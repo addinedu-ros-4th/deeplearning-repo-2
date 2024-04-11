2f063f80fa2eafb7c7ab7f8accc85ab08974d7bb
gudxok <gudxok_@naver.com>
2024-04-12 00:03:53 +0900

    cam bug fix

--- src/pinkla_camera/pinkla_camera.py
+++ src/pinkla_camera/pinkla_camera.py
@@ -18,19 +18,15 @@ class CameraStreamer:
 
     def connect_to_server(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connected = False
         while not connected:
             try:
                 s.connect((self.server_address, self.port))
                 connected = True
-                print("Connected to server")
             except ConnectionRefusedError:
-                print("Waiting for server...")
                 time.sleep(1)
         return s
 
     def send_video_data(self):
-        self.socket = self.connect_to_server()
 
         cam = cv2.VideoCapture(self.cam_index)
         cam.set(3, self.width)
@@ -45,48 +41,25 @@ class CameraStreamer:
                     stringData = data.tostring()
                     self.socket.sendall((str(len(stringData))).encode().ljust(16) + stringData)
                 else:
-                    print("Failed to capture frame from camera.")
                     break
             except Exception as e:
-                self.socket = self.connect_to_server()
+                pass
 
         cam.release()
         self.socket.close()
-        print("release and close ")
 
 def main():
-    server_address = '192.168.0.197'
+    server_address = ''
     port1 = 8485
     port2 = 8584
 
-    # Check the number of arguments
     if len(sys.argv) == 2:
-        # Only one argument, run a single thread for the specified camera index
         cam_index = int(sys.argv[1])
-
         camera1 = CameraStreamer(server_address, port1, cam_index)
-        camera2 = CameraStreamer(server_address, port2, cam_index)
         thread1 = threading.Thread(target=camera1.send_video_data)
-        thread2 = threading.Thread(target=camera2.send_video_data)
         thread1.start()
-        thread2.start()
         thread1.join()
-        thread2.join()
-
-    elif len(sys.argv) == 3:
-        # Two arguments, run threads for both camera indices
-        cam_indices = [int(arg) for arg in sys.argv[1:]]
-
-        camera1 = CameraStreamer(server_address, port1, cam_indices[0])
-        camera2 = CameraStreamer(server_address, port2, cam_indices[1])
-        thread1 = threading.Thread(target=camera1.send_video_data)
-        thread2 = threading.Thread(target=camera2.send_video_data)
-        thread1.start()
-        thread2.start()
-        thread1.join()
-        thread2.join()
     else:
-        print("Usage: python3 camera_stream.py <cam_index1> [<cam_index2>]")
         sys.exit(1)
 
 if __name__ == "__main__":
