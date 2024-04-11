4fe1b2ea18c871bbbca6c7a297062e65478b59cc
gudxok <gudxok_@naver.com>
2024-04-12 00:02:48 +0900

    fix raspi firmware

--- src/pinkla_base/pinkla_raspi.py
+++ src/pinkla_base/pinkla_raspi.py
@@ -2,9 +2,9 @@ import socket
 import serial
 import time
 
-HOST = "192.168.0.100"
+HOST = ""
 PORT = 8090
-port = '/dev/ttyUSB0'
+port = ''
 baud = 9600
 
 
@@ -18,7 +18,6 @@ def get_socket_send_serial(conn, ser):
                 if data_list:
                     data_list.append('\n')
                     data_str = ','.join(map(str, data_list))
-                    # print(data_str)
                     ser.write(data_str.encode())
                 else:
                     raise ValueError
@@ -26,22 +25,15 @@ def get_socket_send_serial(conn, ser):
                 raise ValueError
 
         except Exception as e:
-            # print("Exception: ", e)
             data_list = [0, 0, 5, 0, 0, 0, 0,'\n']
             data_str = ','.join(map(str, data_list))
             ser.write(data_str.encode())
-            conn.close()
-            conn = connect()
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # print('Socket created')
     s.bind((HOST, PORT))
-    # print('Socket bind complete')
     s.listen(10)
-    # print('Socket now listening')
     conn, addr = s.accept()
-    print(conn)
     return conn
 
 def main():
@@ -50,7 +42,6 @@ def main():
     ser = serial.Serial(port, baud)
 
     if conn is not None and ser is not None:
-        print("connected")
         while True:
             try:
                 get_socket_send_serial(conn, ser)
